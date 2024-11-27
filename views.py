191e12551021603a65b4608b342de3d242a5115c
Vatambeduniharika <153808070+Vatambeduniharika@users.noreply.github.com>
2024-11-27 19:08:45 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def kohil(request):
+    return HttpResponse('<h1> captain king kohil will lift the trophy this year</h1>')
